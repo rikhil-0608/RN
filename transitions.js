2d7659353a4bc87e05af886e7ad763eedb262132
rikhil-0608 <rikhilkunta06@gmail.com>
2025-02-03 15:32:48 +0530

    Add files via upload

---
+++ transitions.js
@@ -0,0 +1,17 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, {
+    threshold: 0.5 // Trigger when 50% of element is visible
+});
+
+document.querySelectorAll('.fade-in-right').forEach((element) => {
+    observer.observe(element);
+});
+
+document.querySelectorAll('.fade-in-left').forEach((element) => {
+    observer.observe(element);
+});
